edcd827f0eb33a050ff913a1d08cfabdf8aa6be2
grazzer <edwardsgraham@hotmail.co.uk>
2024-11-01 12:22:42 +0000

    DetailsScreen image anh header parallax

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, ScrollView, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, Image, TouchableOpacity, Dimensions} from 'react-native';
 import Spacer from "../components/Spacer";
 import SafeViewAndroid from "../styleSheets/AndroidSafeArea.js";
 import DetailsHeight from "../components/DetailsHeight";
@@ -9,12 +9,21 @@ import DetailsRow from "../components/DetailsRow";
 import DetailsAbout from "../components/DetailsAbout";
 import { useSelector } from "react-redux";
 import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+import Animated, {
+	interpolate,
+	useAnimatedRef,
+	useAnimatedStyle,
+	useScrollViewOffset
+} from 'react-native-reanimated';
 
 const Icon = createIconSetFromIcoMoon(
     require('../../assets/icomoon/selection.json'),
     'IcoMoon',
     'icomoon.ttf'
 );
+
+const IMG_HEIGHT = 250;
+const { width } = Dimensions.get('window');
 
 export default function DetailScreen({ navigation, route }) {
     const munro = route.params.munro
@@ -31,33 +40,71 @@ export default function DetailScreen({ navigation, route }) {
         }
     },[myClimbsState])
 
+    // animations
+    const scrollRef = useAnimatedRef();
+	const scrollOffset = useScrollViewOffset(scrollRef);
+
+    const imageAnimatedStyle = useAnimatedStyle(() => {
+		return {
+			transform: [
+				{
+					translateY: interpolate(
+						scrollOffset.value,
+						[-IMG_HEIGHT, 0, IMG_HEIGHT],
+						[-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
+					)
+				},
+				{
+					scale: interpolate(scrollOffset.value, [-IMG_HEIGHT, 0, IMG_HEIGHT], [2, 1, 1])
+				}
+			]
+		};
+	});
+
+    const headerAnimatedStyle = useAnimatedStyle(() => {
+		return {
+			opacity: interpolate(scrollOffset.value, [0, IMG_HEIGHT / 1.5], [0, 1])
+		};
+	});
+
+    // <Stack.Screen
+	// 			options={{
+	// 				headerTransparent: true,
+	// 				headerLeft: () => <Text>Back</Text>,
+	// 				headerBackground: () => <Animated.View style={[styles.header, headerAnimatedStyle]} />
+	// 			}}
+	// 		/>
+
 
     return (
         <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
             <View style={styles.container}>
-                <View style={styles.imageContainer}>
-                    <Image
-                        style={styles.container}
-                        source={{uri: 'https://hillsummits.piwigo.com/_datas/6/9/w/69wjfjw5b4/i/uploads/6/9/w/69wjfjw5b4//2014/11/06/20141106210829-ca587d79-me.jpg',}}                           
-                    />
-                </View>
-                <View style={styles.detailsContainer}>
-                    <ScrollView >
-                        <View style={styles.details}>
-                            <Text style={styles.name}>{munro.Name}</Text>
-                            <DetailsRow IconName={'hashtag'} data={munro.Number} size={15} />
-                            <DetailsRow IconName={'location'} data={munro.County} size={15} />
-                            <Spacer size={20} />
-                            <DetailsHeight hight={munro.Drop} seaLevel={munro.Metres} />
-                            <Spacer size={20} />
-                            <DetailsClimbed climbed={true} climbedList={climbedData} munro={munro} navigation={navigation} />
-                            <Spacer size={20} />
-                            <DetailsMap lat={munro.Latitude} lon={munro.Longitude} name={munro.Name} />
-                            <Spacer size={20} />
-                            <DetailsAbout munro={munro} />
-                        </View>
-                    </ScrollView>
-                </View>
+                <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
+                    <View style={styles.imageContainer}>
+                        <Animated.Image
+                            style={[styles.image, imageAnimatedStyle]}
+                            source={{uri: 'https://hillsummits.piwigo.com/_datas/6/9/w/69wjfjw5b4/i/uploads/6/9/w/69wjfjw5b4//2014/11/06/20141106210829-ca587d79-me.jpg',}}                           
+                        />
+                    </View>
+                    <View style={styles.detailsContainer}>
+                            <View style={styles.details}>
+                                <Text style={styles.name}>{munro.Name}</Text>
+                                <DetailsRow IconName={'hashtag'} data={munro.Number} size={15} />
+                                <DetailsRow IconName={'location'} data={munro.County} size={15} />
+                                <Spacer size={20} />
+                                <DetailsHeight hight={munro.Drop} seaLevel={munro.Metres} />
+                                <Spacer size={20} />
+                                <DetailsClimbed climbed={true} climbedList={climbedData} munro={munro} navigation={navigation} />
+                                <Spacer size={20} />
+                                <DetailsMap lat={munro.Latitude} lon={munro.Longitude} name={munro.Name} />
+                                <Spacer size={20} />
+                                <DetailsAbout munro={munro} />
+                            </View>
+                    </View>
+                </Animated.ScrollView>
+
+                <Animated.View style={[styles.header, headerAnimatedStyle]}/>
+
                 <TouchableOpacity
                             style = {styles.button}
                             onPress={() => navigation.goBack()}
@@ -105,5 +152,18 @@ const styles = StyleSheet.create({
     },
     icon: {
         aspectRatio: 1 / 1,
+    },
+    image: {
+        width: width, 
+        height: IMG_HEIGHT
+    },
+    headerContainer: {
+        backgroundColor: '#00000000',
+    },
+    header: {
+        width:width,
+        position: 'absolute',                                          
+        backgroundColor: '#F5F5F5',
+		height: 60,
     }
 });
